f79612ecf5f518df024452abed6e3d1fd8fd256c
Ilari Katajamäki <ilari.katajamaki1@gmail.com>
2022-10-04 14:07:57 +0300

    cahnged the song

--- index.js
+++ index.js
@@ -10,3 +10,31 @@ const audioElement = document.querySelector('audio');
 
 // pass it into the audio context
 const track = audioContext.createMediaElementSource(audioElement);
+
+
+// Select our play button
+const playButton = document.querySelector('button');
+
+playButton.addEventListener('click', () => {
+    // Check if context is in suspended state (autoplay policy)
+    if (audioContext.state === 'suspended') {
+        audioContext.resume();
+    }
+    
+    // Play or pause track depending on state
+    if (playButton.dataset.playing === 'false') {
+        audioElement.play();
+        playButton.dataset.playing = 'true';
+    } else if (playButton.dataset.playing === 'true') {
+        audioElement.pause();
+        playButton.dataset.playing = 'false';
+    }
+}, false);
+
+
+//gain node
+
+const gainNode = audioContext.createGain();
+
+//Audion yhdistäminen
+track.connect(gainNode).connect(audioContext.destination);
