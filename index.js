b43b526f0b8b3af3c684db5ba70c77d4edb850d2
Ilari Katajamäki <ilari.katajamaki1@gmail.com>
2022-10-04 15:54:02 +0300

    broken start button

--- index.js
+++ index.js
@@ -14,42 +14,42 @@ button.addEventListener('click',()=>{
     const track = audioContext.createMediaElementSource(audioElement);
     
     
+    
+    // Select our play button
+    const playButton = document.querySelector('button');
+    
+    playButton.addEventListener('click', () => {
+        // Check if context is in suspended state (autoplay policy)
+        if (audioContext.state === 'suspended') {
+            audioContext.resume();
+        }
+        
+        // Play or pause track depending on state
+        if (playButton.dataset.playing === 'false') {
+            audioElement.play();
+            playButton.dataset.playing = 'true';
+        } else if (playButton.dataset.playing === 'true') {
+            audioElement.pause();
+            playButton.dataset.playing = 'false';
+        }
+    }, false);
+    
+    
+    //gain node
+    
+    const gainNode = audioContext.createGain();
+    
+    const volumeControl = document.querySelector('#volume');
+    
+    volumeControl.addEventListener('input',() =>{
+        gainNode.gain.value = volumeControl.value;
+    }, false);
+    
+    //Audion yhdistäminen
+    track.connect(gainNode)
+    gainNode.connect(audioContext.destination);
 }, false)
 
-
-// Select our play button
-const playButton = document.querySelector('button');
-
-playButton.addEventListener('click', () => {
-    // Check if context is in suspended state (autoplay policy)
-    if (audioContext.state === 'suspended') {
-        audioContext.resume();
-    }
-    
-    // Play or pause track depending on state
-    if (playButton.dataset.playing === 'false') {
-        audioElement.play();
-        playButton.dataset.playing = 'true';
-    } else if (playButton.dataset.playing === 'true') {
-        audioElement.pause();
-        playButton.dataset.playing = 'false';
-    }
-}, false);
-
-
-//gain node
-
-const gainNode = audioContext.createGain();
-
-const volumeControl = document.querySelector('#volume');
-
-volumeControl.addEventListener('input',() =>{
-    gainNode.gain.value = volumeControl.value;
-}, false);
-
-//Audion yhdistäminen
-track.connect(gainNode)
-gainNode.connect(audioContext.destination);
 
 function Click(){
     
