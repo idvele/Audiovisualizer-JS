11348d1c90c2b5be46f7140d0c94a293a37f05d9
Ilari Katajamäki <ilari.katajamaki1@gmail.com>
2022-10-04 13:58:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Det funkkar!");
+
+// for legacy browsers
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+const audioContext = new AudioContext();
+
+// get the audio element
+const audioElement = document.querySelector('audio');
+
+// pass it into the audio context
+const track = audioContext.createMediaElementSource(audioElement);
