006ac7a7f20d7e83207e1af0782989c291714bad
Ilari Katajamäki <ilari.katajamaki1@gmail.com>
2022-10-06 10:36:29 +0300

    changed backgroundcolor, site works on mobile

--- script.js
+++ script.js
@@ -65,19 +65,29 @@ const dataArray = new Uint8Array(bufferLength);
 const barWidth = canvas.width/bufferLength;
 let barHeight;
 
+//Animoi visualisaattori
+
 function animate(){
     x=0;
     ctx.clearRect(0,0,canvas.width, canvas.height);
     analyser. getByteFrequencyData(dataArray);
-    for (let i=0; i< bufferLength; i++){
-        barHeight= dataArray[i]*2;
-        ctx.fillStyle = 'blue';
-        ctx.fillRect(x, canvas.height-barHeight, barWidth, barHeight);
-        x+= barWidth;
-    }
+    drawVisualizer(bufferLength, x, barWidth, barHeight, dataArray);
     requestAnimationFrame(animate);
 }
 animate()
+
+//piirrä visualisaattori
+
+function drawVisualizer(bufferLength, x, barWidth, barHeight, dataArray){
+    for (let i=0; i< bufferLength; i++){
+        barHeight= dataArray[i]*2;
+    
+        ctx.fillStyle = 'green';
+        ctx.fillRect(x, canvas.height-barHeight, barWidth, barHeight);
+        x+= barWidth;
+    }
+    
+}
 
 //Audion yhdistäminen
 track.connect(analyser)
